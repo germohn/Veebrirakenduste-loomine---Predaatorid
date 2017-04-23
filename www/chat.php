6335fc849b7ab1e77f1c7dd2f73283f7ccad2af5
germo <germohn@gmail.com>
2017-04-23 19:46:02 +0100

    check 1,2,3

---
+++ www/chat.php
@@ -0,0 +1,64 @@
+<?php
+include 'language.php';
+include 'cookie.php';
+
+
+?>
+<head>
+    <title>Predaator</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/lib/bootstrap/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<div class="jumbotron">
+    <div class="container text-center">
+        <h1><?php echo $lang['PAGE_TITLE']; ?></h1>
+        <p><?php echo $lang['HEADER_TITLE']; ?></p>
+    </div>
+</div>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">PANE Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="login.php"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <div>
+        <!--        push data output-->
+    </div>
+    <div>
+<!--    input text-->
+        <form action="#" method="post">
+            <input type="text" name="input">
+            <input type="submit" name="submit"  >
+        </form>
+    </div>
+</div>
+<footer class="container-fluid text-center">
+    <p>Predaator Copyright</p>
+</footer>
+
+</body>
+</html>
